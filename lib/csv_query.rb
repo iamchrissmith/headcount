62b2dc99bbf2fd9f214b7f1e78b0110e9b62e4a0
Sam Lim <limsammy1@gmail.com>
2017-04-10 17:55:00 -0600

    Wrote methods #csv_to_hash, format_percent, and sanitize (not all the way finished yet, just truncating the floats): all tests passing, all code being tested

--- lib/csv_query.rb
+++ lib/csv_query.rb
@@ -1,13 +1,13 @@
 require 'csv'
+require 'pry'
 
 module CSVQuery
   def load_file(file_name)
     CSV.open file_name, headers:true, header_converters: :symbol
   end
 
-  def parse_file(file_name)
-    contents = load_file(file_name)
-    contents.sanitize # create sanitization method as illustrated on whiteboard
+  def parse_file(file_name, header)
+    sanitize(load_file(file_name), header)
   end
 
   def get_column(file_name, header)
@@ -16,5 +16,26 @@ module CSVQuery
     contents.map do |row|
       row[header]
     end
+  end
+
+  def csv_to_hash(file_name)
+    contents = load_file(file_name)
+
+    contents.to_a.map {|row| row.to_hash } #returns array of hashes, each hash storing data relevant to the district
+  end
+
+  def sanitize(file_name, header)
+    if header == :data && get_column(file_name, header)[0].include?(".")
+      data = []
+      csv_to_hash(file_name).each do |hash|
+        hash[:data] = format_percent(hash[:data])
+        data << hash
+      end
+    end
+    data
+  end
+
+  def format_percent(number)
+    number.to_f.round(3)
   end
 end
--- test/csv_loader_test.rb
+++ test/csv_loader_test.rb
@@ -24,4 +24,15 @@ class CSVQueryTest < Minitest::Test
   def test_csv_to_hash_returns_hash_of_data
     assert_equal 1086, DummyClass.new.csv_to_hash("./data/Pupil enrollment.csv").count
   end
+
+  def test_format_percent_creates_float_and_rounds_to_3_decimals
+    sample_num = 2.545195857435
+
+    assert_instance_of Float, DummyClass.new.format_percent(sample_num)
+    assert_equal 2.545, DummyClass.new.format_percent(sample_num)
+  end
+
+  def test_sanitize_data_header
+    assert_equal 0.101, DummyClass.new.sanitize("./data/Special education.csv", :data)[4][:data]
+  end
 end
