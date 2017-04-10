b58111107636f60798d6798073d45bb73df76891
Sam Lim <limsammy1@gmail.com>
2017-04-10 17:24:33 -0600

    added test for #csv_to_hash, passes

--- test/csv_loader_test.rb
+++ test/csv_loader_test.rb
@@ -7,7 +7,7 @@ end
 
 class CSVQueryTest < Minitest::Test
   def test_load_file_method
-  assert_respond_to(DummyClass.new, :load_file)
+    assert_respond_to(DummyClass.new, :load_file)
   end
 
   def test_load_file_returns_csv_object_of_loaded_file
@@ -15,10 +15,13 @@ class CSVQueryTest < Minitest::Test
   end
 
   def test_get_column_returns_indiciated_column
-    dummy = DummyClass.new
-    column = dummy.get_column("./data/Special education.csv", :location)
+    column = DummyClass.new.get_column("./data/Special education.csv", :location)
     column = column[0].to_s
 
     assert_includes "Colorado", column
+  end
+
+  def test_csv_to_hash_returns_hash_of_data
+    assert_equal 1086, DummyClass.new.csv_to_hash("./data/Pupil enrollment.csv").count
   end
 end
